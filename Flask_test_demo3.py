834ab8a886821c3412620cfbae4b9b799810bb36
kowloontong <40264670+kowloontong@users.noreply.github.com>
2020-01-23 21:02:36 +0800

    Add files via upload

---
+++ Flask_test_demo3.py
@@ -0,0 +1,101 @@
+#coding=utf-8
+from flask import Flask, render_template, Response,request, flash,redirect,url_for
+#导入wtf扩展的表单类
+from flask_wtf import FlaskForm
+#导入自定义表单需要的字段
+from wtforms import SubmitField,StringField,PasswordField
+#导入wtf扩展提供的表单验证器
+from wtforms.validators import DataRequired,EqualTo
+import mqtt_client1
+import sys
+#import threading
+
+#-------------------------------------------------------------mqtt--------------------------------------------------------------------------
+try:
+    my_client1
+except:
+    my_client1 = mqtt_client1.Mqtt_client1()
+
+try:
+    my_client2
+except:
+    #print(2222)
+    my_client2 = mqtt_client1.Mqtt_client2()
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='heima'
+start_flag = False
+
+
+#自定义表单类，文本字段、密码字段、提交按钮
+# 需要自定义一个表单类
+class RegisterForm(FlaskForm):
+    username = StringField(u'用户名:', validators=[DataRequired()])
+    password = PasswordField(u'密码:', validators=[DataRequired()])
+    password2 = PasswordField(u'确认密码:', validators=[DataRequired(), EqualTo(u'password', u'密码输入不一致')])
+    input = SubmitField(u'提交')
+
+#定义根路由视图函数，生成表单对象，获取表单数据，进行表单数据验证
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    register_form = RegisterForm()
+
+    if request.method == 'POST':
+        # 调用validate_on_submit方法, 可以一次性执行完所有的验证函数的逻辑
+        if register_form.validate_on_submit():
+            # 进入这里就表示所有的逻辑都验证成功
+            username = request.form.get('username')
+            password = request.form.get('password')
+            password2 = request.form.get('password2')
+            #print (username)
+            return redirect('/stream')
+
+        else:
+            #message = register_form.errors.get('password2')[0]
+            #flash(message)
+            flash('参数有误')
+
+    return render_template('sign_in.html', form=register_form)
+
+@app.route("/start_display")
+def action1():    # button
+    global start_flag
+    start_flag = True
+    print('start display!!!')
+    return "Success"#, {'Content-Type': 'text/plain'}
+
+@app.route("/stop_display")
+def action2():    # button
+    global start_flag
+    start_flag = False
+    print('stop display!!!')
+    return "Success"#, {'Content-Type': 'text/plain'}
+
+@app.route('/stream')  # 主页
+def index():
+    # jinja2模板，具体格式保存在index.html文件中
+    return render_template('stream.html')
+
+def gen():
+    global start_flag
+    while True:
+        my_client1.client.loop()
+        if start_flag == False:
+            continue
+        if my_client1.img_str == None:
+            continue
+        frame = my_client1.drawSth_OnJpgFrame(my_client1.get_cvFrame(),my_client2.rect_list)#.tobytes()
+        #print('frame:',frame)
+        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')  # 这个地址返回视频流响应
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')   
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ mqtt_client1.py
@@ -0,0 +1,111 @@
+import cv2
+import time
+import paho.mqtt.client as mqtt
+import perception_image2d_pb2
+import PerceptionResult_pb2
+import numpy as np
+
+HOST = "127.0.0.1"#"39.106.157.212"
+MQTT_PORT = 1883
+ALIVE = 60
+topic_sub1 = "/img/test"
+topic_sub2 = "/img/preception_result"
+
+class Mqtt_client1():
+    def __init__(self):
+        self.client = None
+        self.img_str = None
+        self.init_client()
+
+    def on_connect(self,client, userdata, flags, rc):
+	    print("Connected with result code " + str(rc)+" client1")
+	    client.subscribe(topic_sub1)
+
+    def on_message(self,client, userdata, msg):
+        pimg_2d = perception_image2d_pb2.PerceptionRawImage2D()
+        if pimg_2d.ParseFromString(msg.payload):
+            self.img_str = pimg_2d.data
+        else:
+            print("解析失败")
+
+    def init_client(self):
+        #声明客户端
+        self.client = mqtt.Client()
+        #连接
+        self.client.connect(HOST, MQTT_PORT, ALIVE)
+        #两个回调函数
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def get_cvFrame(self):
+        np_arr = np.fromstring(self.img_str,np.uint8)
+        cv_frame = cv2.imdecode(np_arr,cv2.IMREAD_COLOR)
+        return cv_frame
+
+    def drawSth_OnJpgFrame(self,cv_frame,rect_list):
+        cv2.putText(cv_frame,"test",(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+        if rect_list!=None:
+            cv2.rectangle(cv_frame, (rect_list[0], rect_list[1]), (rect_list[0]+rect_list[2], rect_list[1]+rect_list[3]),(255,255,0), 2)
+        ret,jpeg = cv2.imencode('.jpg',cv_frame)
+        return jpeg.tobytes()
+
+class Mqtt_client2():
+    def __init__(self):
+        self.client = None
+        self.rect_list = None
+        self.init_client()
+
+
+    def on_connect(self,client, userdata, flags, rc):
+	    print("Connected with result code " + str(rc)+" client2")
+	    client.subscribe(topic_sub2)
+
+    def on_message(self,client, userdata, msg):
+        pr_msg = PerceptionResult_pb2.PerceptionResult()
+        if pr_msg.ParseFromString(msg.payload):
+            self.rect_list = [pr_msg.x,pr_msg.y,pr_msg.w,pr_msg.h]
+            #print('client2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            
+        else:
+            print("解析失败")
+
+    def init_client(self):
+        #声明客户端
+        self.client = mqtt.Client()
+        #连接
+        self.client.connect(HOST, MQTT_PORT, ALIVE)
+        #两个回调函数
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.loop_start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
