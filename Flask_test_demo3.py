da535b842804ed38e8a973fef9ba26f474d0df7b
kowloontong <40264670+kowloontong@users.noreply.github.com>
2020-01-29 11:53:09 +0800

    Update Flask_test_demo3.py

--- Flask_test_demo3.py
+++ Flask_test_demo3.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-from flask import Flask, render_template, Response,request, flash,redirect,url_for
+from flask import Flask, render_template, Response,request, flash,redirect,url_for,session
 #导入wtf扩展的表单类
 from flask_wtf import FlaskForm
 #导入自定义表单需要的字段
@@ -10,7 +10,7 @@ import mqtt_client1
 import sys
 #import threading
 
-#-------------------------------------------------------------mqtt--------------------------------------------------------------------------
+'''#-------------------------------------------------------------mqtt--------------------------------------------------------------------------
 try:
     my_client1
 except:
@@ -22,11 +22,12 @@ except:
     #print(2222)
     my_client2 = mqtt_client1.Mqtt_client2()
 #-------------------------------------------------------------------------------------------------------------------------------------------
-
+'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='heima'
 start_flag = False
+quit_flag  = False
 
 
 #自定义表单类，文本字段、密码字段、提交按钮
@@ -34,7 +35,7 @@ start_flag = False
 class RegisterForm(FlaskForm):
     username = StringField(u'用户名:', validators=[DataRequired()])
     password = PasswordField(u'密码:', validators=[DataRequired()])
-    password2 = PasswordField(u'确认密码:', validators=[DataRequired(), EqualTo(u'password', u'密码输入不一致')])
+    password2 = PasswordField(u'确认密码: ', validators=[DataRequired(), EqualTo(u'password', u'密码输入不一致')])
     input = SubmitField(u'提交')
 
 #定义根路由视图函数，生成表单对象，获取表单数据，进行表单数据验证
@@ -49,8 +50,13 @@ def form():
             username = request.form.get('username')
             password = request.form.get('password')
             password2 = request.form.get('password2')
+            if username=='0000' and password=='0000' and password2=='0000':
+                session['success'] = 'xxx'
+                return redirect('/stream')
+            else:
+                flash('参数有误')
             #print (username)
-            return redirect('/stream')
+            #return redirect('/stream')
 
         else:
             #message = register_form.errors.get('password2')[0]
@@ -61,13 +67,20 @@ def form():
 
 @app.route("/start_display")
 def action1():    # button
+    info = session.get('success')
+    if not info:
+        return redirect('/')
     global start_flag
+
     start_flag = True
     print('start display!!!')
     return "Success"#, {'Content-Type': 'text/plain'}
 
 @app.route("/stop_display")
 def action2():    # button
+    info = session.get('success')
+    if not info:
+        return redirect('/')
     global start_flag
     start_flag = False
     print('stop display!!!')
@@ -75,13 +88,22 @@ def action2():    # button
 
 @app.route('/stream')  # 主页
 def index():
+    info = session.get('success')
+    if not info:
+        return redirect('/')
+    global quit_flag
+    quit_flag = False
     # jinja2模板，具体格式保存在index.html文件中
     return render_template('stream.html')
 
 def gen():
-    global start_flag
+    global start_flag,quit_flag
+    my_client1 = mqtt_client1.Mqtt_client1()
+    my_client2 = mqtt_client1.Mqtt_client2()
     while True:
         my_client1.client.loop()
+        if quit_flag == True:
+            break
         if start_flag == False:
             continue
         if my_client1.img_str == None:
@@ -91,11 +113,26 @@ def gen():
         # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    del my_client1
+    del my_client2
+    print('quit')
+    #print('myclient1 ref num:',sys.getrefcount(Mqtt_client1()))
+    
 
 @app.route('/video_feed')  # 这个地址返回视频流响应
 def video_feed():
+    info = session.get('success')
+    if not info:
+        return redirect('/')
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')   
+
+@app.route('/quit') 
+def quit():
+    global quit_flag
+    quit_flag = True
+    session.clear()
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
